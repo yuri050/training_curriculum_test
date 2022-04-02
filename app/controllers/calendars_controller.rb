3e4be025b2b568e25b5dc3026ea3ec55b1948711
yuri050 <cheb.owo1182@gmail.com>
2022-04-02 17:09:30 +0900

    Issue2

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -2,39 +2,40 @@ class CalendarsController < ApplicationController
 
   # １週間のカレンダーと予定が表示されるページ
   def index
-    getWeek
+    get_week
     @plan = Plan.new
   end
 
   # 予定の保存
   def create
-    Plan.create(plan_params)
-    redirect_to action: :index
+    Plan.create(plan_params)  # Planという箱に(plan_params)というデータをcreate(保存する)
+    redirect_to action: :index  # usersコントローラのindexアクションへリダイレクト
   end
 
-  private
+  private　# クラス外から呼び出すことのできないメソッド
 
   def plan_params
-    params.require(:calendars).permit(:date, :plan)
+    params.require(:calendars).permit(:date, :plan) 
+    # パラメーター(params)の中からcalendarsを探す(requireする)→calendarsの中からdateとplanを探す(permitする)
   end
 
-  def getWeek
+  def get_week
     wdays = ['(日)','(月)','(火)','(水)','(木)','(金)','(土)']
 
     # Dateオブジェクトは、日付を保持しています。下記のように`.today.day`とすると、今日の日付を取得できます。
     @todays_date = Date.today
-    # 例)　今日が2月1日の場合・・・ {Date.today.day: '1日'}
+    # 例)　今日が2月1日の場合・・・ {Date.today.day: '1日
 
     @week_days = []
 
-    plans = Plan.where(date: @todays_date..@todays_date + 6)
+    plans = Plan.where(date: @todays_get_date..@todays_get_date + 6)
 
-    7.times do |x|
+    7.times do |x| # 7 から１つずつ順番に要素を取り出し変数 x に代入
       today_plans = []
-      plans.each do |plan|
-        today_plans.push(plan.plan) if plan.date == @todays_date + x
+      plans.each do |plan| # plans から１つずつ順番に要素を取り出し変数 plan に代入
+        today_plans.push(plan.plan) if plan.date == @todays_get_date + x
       end
-      days = {month:  (@todays_date + x).month, date:  (@todays_date+x).day, plans: today_plans}
+      days = {month:  (@todays_get_date + x).month, date:  (@todays_get_date+x).day, plans: today_plans}
       @week_days.push(days)
     end
 
